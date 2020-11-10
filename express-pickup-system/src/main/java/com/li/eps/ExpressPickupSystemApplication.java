025c2446f0430145715a445ea9d4148292a0ef92
张华朋 <huapeng.zhang@hand-china.com>
2020-11-10 14:02:53 +0800

    [ADD] eps

---
+++ express-pickup-system/src/main/java/com/li/eps/ExpressPickupSystemApplication.java
@@ -0,0 +1,13 @@
+package com.li.eps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpressPickupSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpressPickupSystemApplication.class, args);
+	}
+
+}
---
+++ express-pickup-system/src/test/java/com/li/eps/ExpressPickupSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.li.eps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpressPickupSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
